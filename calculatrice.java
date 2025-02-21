535a468414236de2ccb474ca4807652208436916
nawres <nawresbensaid8@gmail.com>
2025-02-21 21:45:53 +0100

    modification

--- calculatrice.java
+++ calculatrice.java
@@ -1,75 +1,82 @@
 import java.util.Scanner;
-class Calculatrice{
-    public double addition(double a,double b){
+
+class Calculatrice {
+
+    public double addition(double a, double b) {
         return a + b;
     }
-    public double soustraction(double a,double b){
+
+    public double soustraction(double a, double b) {
         return a - b;
     }
-    public double division(double a,double b){
-        return a - b;
-    }
-     public double multiplication(double a,double b){
+
+    public double multiplication(double a, double b) {
         return a * b;
     }
-    
+
+    public double division(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("Erreur : Division par zéro !");
+        }
+        return a / b;
+    }
 }
-class test{
-    public static void main (string[]args){
-        public class Test {
+
+public class Test {
     public static void main(String[] args) {
-        Calculatrice calc = new Calculatrice();
         Scanner scanner = new Scanner(System.in);
-        boolean continuer = true;
+        Calculatrice calculatrice = new Calculatrice();
 
-        System.out.println("Bienvenue dans la calculatrice Java !");
+        System.out.println("Bienvenue dans la calculatrice !");
 
-        while (continuer) {
+        while (true) { 
+            System.out.println("\nChoisissez une opération :");
+            System.out.println("1. Addition (+)");
+            System.out.println("2. Soustraction (-)");
+            System.out.println("3. Multiplication (*)");
+            System.out.println("4. Division (/)");
+            System.out.println("5. Quitter");
+            System.out.print("Votre choix : ");
+            int choix = scanner.nextInt();
+
+            if (choix == 5) {
+                System.out.println("Merci d'avoir utilisé la calculatrice. Au revoir !");
+                break; 
+            }
+
+            System.out.print("Entrez le premier nombre : ");
+            double nombre1 = scanner.nextDouble();
+            System.out.print("Entrez le deuxième nombre : ");
+            double nombre2 = scanner.nextDouble();
+
+            double resultat = 0;
             try {
-                System.out.println("\nChoisissez une opération :");
-                System.out.println("1. Addition");
-                System.out.println("2. Soustraction");
-                System.out.println("3. Multiplication");
-                System.out.println("4. Division");
-                int choix = scanner.nextInt();
-
-
-                System.out.print("Entrez le premier nombre : ");
-                double a = scanner.nextDouble();
-                System.out.print("Entrez le deuxième nombre : ");
-                double b = scanner.nextDouble();
-
-                double resultat = 0;
-
                 switch (choix) {
                     case 1:
-                        resultat = calc.addition(a, b);
+                        resultat = calculatrice.addition(nombre1, nombre2);
+                        System.out.println("Résultat de l'addition : " + resultat);
                         break;
                     case 2:
-                        resultat = calc.soustraction(a, b);
+                        resultat = calculatrice.soustraction(nombre1, nombre2);
+                        System.out.println("Résultat de la soustraction : " + resultat);
                         break;
                     case 3:
-                        resultat = calc.multiplication(a, b);
+                        resultat = calculatrice.multiplication(nombre1, nombre2);
+                        System.out.println("Résultat de la multiplication : " + resultat);
                         break;
                     case 4:
-                        resultat = calc.division(a, b);
+                        resultat = calculatrice.division(nombre1, nombre2);
+                        System.out.println("Résultat de la division : " + resultat);
                         break;
                     default:
                         System.out.println("Choix invalide. Veuillez réessayer.");
-                        continue;
+                        break;
                 }
-
-                System.out.println("Résultat : " + resultat);
-
             } catch (ArithmeticException e) {
                 System.out.println(e.getMessage());
-            } catch (Exception e) {
-                System.out.println("Erreur : Veuillez entrer des valeurs valides.");
-                scanner.nextLine(); // Nettoyer le buffer du scanner
             }
         }
 
         scanner.close();
     }
 }
-    }
